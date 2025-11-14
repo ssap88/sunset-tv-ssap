6c37037b9f60c94655311ee5ba37b837411277de
ssap88 <ssap88@gmail.com>
2025-11-13 18:28:02 -0600

    Update SunsetLanding.jsx

--- src/SunsetLanding.jsx
+++ src/SunsetLanding.jsx
@@ -179,7 +179,7 @@ export default function SunsetLanding() {
   }}
 >
   <img
-    src="https://ibb.co/HLfwcRHV"
+    src="[url=https://ibb.co/HLfwcRHV][img]https://i.ibb.co/HLfwcRHV/Screenshot-20251113-181715-Amazon-Shopping.jpg[/img][/url]"
     alt="Fire TV Stick"
     style={{
       width: "90px",
